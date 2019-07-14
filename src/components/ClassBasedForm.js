df9123d6f0638655dc3a5fc44a2ee195ab26f234
Brian Liotti <Brian Liotti>
2019-07-14 12:24:53 -0400

    init

--- src/components/ClassBasedForm.js
+++ src/components/ClassBasedForm.js
@@ -1,60 +1,54 @@
-import React from 'react'
-import {
-  Form, FormGroup, Input,
-  Label, Col, Button,
-} from 'reactstrap'
+import React, { useState } from 'react'
+import { Form, FormGroup, Input, Label, Col, Button } from 'reactstrap'
 
-export default class ClassBasedForm extends React.Component {
-  constructor(props) {
-    super(props)
-    this.state = {
-      email: '',
-      password: '',
-    }
-    this.handleSubmit = this.handleSubmit.bind(this);
+export default function ClassBasedForm() {
+  const [email, setEmail] = useState('')
+  const [password, setPassword] = useState('')
+
+  const handleSubmit = e => {
+    e.preventDefault()
+    console.log(email, password)
   }
 
-  handleSubmit(e) {
-    e.preventDefault();
-    console.log(this.state);
-  }
-
-  render() {
-    return (
-      <Form onSubmit={ this.handleSubmit }>
-        <h1>Class-Based Form</h1>
-        <FormGroup row>
-          <Label for="exampleEmail" sm={ 2 }>Email</Label>
-          <Col sm={ 8 }>
-            <Input
-              type="email"
-              name="email"
-              id="exampleEmail"
-              placeholder="email"
-              value={ this.state.email }
-              onChange={ (event) => this.setState({ email: event.target.value }) }
-            />
-          </Col>
-        </FormGroup>
-        <FormGroup row>
-          <Label for="examplePassword" sm={ 2 }>Password</Label>
-          <Col sm={ 8 }>
-            <Input
-              type="password"
-              name="password"
-              id="examplePassword"
-              placeholder="password"
-              value={ this.state.password }
-              onChange={ (event) => this.setState({ password: event.target.value })}
-            />
-          </Col>
-        </FormGroup>
-        <FormGroup check row>
-          <Col sm={ { size: 10, offset: 8 } }>
-            <Button>Submit</Button>
-          </Col>
-        </FormGroup>
-      </Form>
-    )
-  }
-};
+  return (
+    <Form onSubmit={handleSubmit}>
+      <h1>Class-Based Form</h1>
+      <FormGroup row>
+        <Label for="exampleEmail" sm={2}>
+          Email
+        </Label>
+        <Col sm={8}>
+          <Input
+            type="email"
+            name="email"
+            id="exampleEmail"
+            placeholder="email"
+            value={email}
+            onChange={event => setEmail(event.target.value)}
+          />
+        </Col>
+      </FormGroup>
+      <FormGroup row>
+        <Label for="examplePassword" sm={2}>
+          Password
+        </Label>
+        <Col sm={8}>
+          <Input
+            type="password"
+            name="password"
+            id="examplePassword"
+            placeholder="password"
+            autoComplete=""
+            value={password}
+            onChange={event => setPassword(event.target.value)}
+          />
+        </Col>
+      </FormGroup>
+      <FormGroup check row>
+        <Col sm={{ size: 10, offset: 8 }}>
+          <Button>Submit</Button>
+        </Col>
+      </FormGroup>
+    </Form>
+  )
+}
